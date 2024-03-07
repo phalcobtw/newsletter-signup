7774fc8fd35bceda64de5fe5188182af01bd6147
phalcobtw <laxz_16@hotmail.com>
2024-03-06 17:21:44 -0700

    cambia span por email introducido

--- js/script.js
+++ js/script.js
@@ -4,22 +4,25 @@ const error = document.querySelector(".error-text");
 const signupcont = document.querySelector(".signup-container");
 const success = document.querySelector(".success-container");
 const dismiss = document.querySelector("#dismiss");
+const email = document.querySelector(".success-email");
 
 button.addEventListener("click", function () {
   if (inputtext.value === "" || !isValidEmail(inputtext.value)) {
+    /* Validacion no pasada, se muestra error */
     error.style.display = "block";
     inputtext.style.backgroundColor = "hsl(7, 75%, 85%)";
     inputtext.style.borderColor = "hsl(4, 100%, 67%)";
     inputtext.style.color = "hsl(4, 100%, 67%)";
   } else {
     // Si pasa las validaciones anteriores, el email es válido
-    /* alert("valido"); */
     signupcont.style.display = "none";
     success.style.display = "grid";
+    email.innerText = inputtext.value;
   }
 });
 
 dismiss.addEventListener("click", function () {
+  /* Se cierra la ventana de success y se muestra denuevo el signup */
   signupcont.style.display = "flex";
   success.style.display = "none";
 });
